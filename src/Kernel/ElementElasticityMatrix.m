1b9819f92573e2247c62b710d022639a34f1e37c
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2020-10-21 13:28:05 +0200

    Update 1

--- src/Kernel/ElementElasticityMatrix.m
+++ src/Kernel/ElementElasticityMatrix.m
@@ -5,23 +5,17 @@ function D = ElementElasticityMatrix()
 	var = zeros(eleType_.numStressComponents);
 	switch domainType_
 		case '2D'
-			D = [
-				HL	var	var	var
-				var	HL	var	var
-				var	var	HL	var
-				var	var	var	HL
-			];		
+			D = zeros(12);
+			for ii=1:4
+				index = (ii-1)*3+1:ii*3;
+				D(index,index) = HL;
+			end
 		case '3D'
-			D = [
-				HL	var	var	var	var	var	var	var
-				var	HL	var	var	var	var	var	var
-				var	var	HL	var	var	var	var	var
-				var	var	var	HL	var	var	var	var
-				var	var	var	var	HL	var	var	var
-				var	var	var	var	var	HL	var	var
-				var	var	var	var	var	var	HL	var
-				var	var	var	var	var	var	var	HL
-			];		
+			D = zeros(48);
+			for ii=1:8
+				index = (ii-1)*6+1:ii*6;
+				D(index,index) = HL;
+			end						
 	end
 	D = sparse(D);
 end
