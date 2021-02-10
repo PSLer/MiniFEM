38afb0a16c698a7245cbc13d4e7597dd657ce367
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2021-02-10 17:20:22 +0100

    Update 1

--- src/Kernel/CreateModelTopOptiSrc.m
+++ src/Kernel/CreateModelTopOptiSrc.m
@@ -43,8 +43,8 @@ function CreateModelTopOptiSrc(srcName, extractThreshold, featureSize)
 			vtxLowerBound_ = [0 0 0];
 			if isempty(featureSize), featureSize = max([nelx_ nely_ nelz_]); end
 			vtxUpperBound_ = featureSize*[nelx_ nely_ nelz_]/max([nelx_ nely_ nelz_]);
-vtxLowerBound_ = [-0.996787000000000	-0.764997000000000	-0.986229000000000];
-vtxUpperBound_ = [0.998007000000000	0.769461000000000	0.976411000000000];		%%for vis2021_bunny2	
+% vtxLowerBound_ = [-0.996787000000000	-0.764997000000000	-0.986229000000000];
+% vtxUpperBound_ = [0.998007000000000	0.769461000000000	0.976411000000000];		%%for vis2021_bunny2	
 	end
 	fclose(fid);
 	opt_CUTTING_DESIGN_DOMAIN_ = 'ON';
