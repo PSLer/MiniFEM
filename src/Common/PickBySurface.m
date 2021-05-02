f7cb8770853add264db1fa7c44dfc49273d72306
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2021-05-02 22:30:36 +0200

    Update 1

--- src/Common/PickBySurface.m
+++ src/Common/PickBySurface.m
@@ -1,4 +1,4 @@
-function PickBySurface(constantDir, varargin)
+function PickBySurface(constantDir, opt, varargin)
 	global eleType_;
 	global nodeCoords_;
 	global boundaryNodes_;
@@ -7,7 +7,10 @@ function PickBySurface(constantDir, varargin)
 	if strcmp(eleType_.eleName, 'Plane133') || strcmp(eleType_.eleName, 'Plane144')
 		warning('Only Work with non-2D Mesh!');
 	end
-	if 1==nargin, surfRelaxFactor = 0; else, surfRelaxFactor = varargin{1}; end
+	if ~(1==opt || 0==opt || -1==opt), warning('Wrong Input!'); return; end
+	if 0==opt
+		if 2==nargin, surfRelaxFactor = 0; else, surfRelaxFactor = varargin{1}; end
+	end
 	dcm_obj = datacursormode;
 	info_struct = getCursorInfo(dcm_obj);
 	if isempty(info_struct)
@@ -25,13 +28,24 @@ function PickBySurface(constantDir, varargin)
 	if 1~=length(constantDir), error('Wrongly Defined Line Direction!'); end
 	switch constantDir
 		case 'X'
-			nodesOnLine = find(abs(boundaryNodeCoords(:,1)-tarNode(1))<=surfRelaxFactor);
+			refNodePool = boundaryNodeCoords(:,1);
+			refPos = tarNode(1);	
 		case 'Y'
-			nodesOnLine = find(abs(boundaryNodeCoords(:,2)-tarNode(2))<=surfRelaxFactor);
+			refNodePool = boundaryNodeCoords(:,2);
+			refPos = tarNode(2);		
 		case 'Z'
-			nodesOnLine = find(abs(boundaryNodeCoords(:,3)-tarNode(3))<=surfRelaxFactor);			
+			refNodePool = boundaryNodeCoords(:,3);
+			refPos = tarNode(3);			
 		otherwise
 			error('Wrongly Defined Line Direction!');
+	end
+	switch opt
+		case 1
+			nodesOnLine = find(refNodePool>=refPos);
+		case 0
+			nodesOnLine = find(abs(refNodePool-refPos)<=surfRelaxFactor);
+		case -1
+			nodesOnLine = find(refNodePool<=refPos);
 	end
 	hdPickedNode_(end+1) = plot3(boundaryNodeCoords(nodesOnLine,1), boundaryNodeCoords(nodesOnLine,2), ...
 		boundaryNodeCoords(nodesOnLine,3), 'xr', 'LineWidth', 2, 'MarkerSize', 8);
