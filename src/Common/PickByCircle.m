e6cc1d159d7b180b2da67a72880a725817ee5e3f
Junpeng-Wang-TUM <47067711+Junpeng-Wang-TUM@users.noreply.github.com>
2021-05-26 18:48:37 +0200

    Update 1

--- src/Common/PickByCircle.m
+++ src/Common/PickByCircle.m
@@ -1,21 +1,40 @@
-function PickByCircle()
+function PickByCircle(varargin)
 	global eleType_;
 	global nodeCoords_;
 	global boundaryNodes_;
 	global pickedNodeCache_;
 	global hdPickedNode_;
-	if size(pickedNodeCache_, 1) < 2, 
-		warning('There MUST be at lease TWO Picked Nodes Available!'); return;
-	end
-	if strcmp(eleType_.eleName, 'Shell133') || strcmp(eleType_.eleName, 'Shell144')
-		boundaryNodeCoords = nodeCoords_;
+	
+	if 0==nargin
+		if size(pickedNodeCache_, 1) < 2, 
+			warning('There MUST be at lease TWO Picked Nodes Available!'); return;
+		end	
+		if strcmp(eleType_.eleName, 'Shell133') || strcmp(eleType_.eleName, 'Shell144')
+			boundaryNodeCoords = nodeCoords_;
+		else
+			boundaryNodeCoords = nodeCoords_(boundaryNodes_,:);
+		end
+		ctr = boundaryNodeCoords(pickedNodeCache_(end-1),:);
+		radi = norm(ctr-boundaryNodeCoords(pickedNodeCache_(end),:));
+		nodesWithinCircle = find(vecnorm(ctr-boundaryNodeCoords,2,2)<=radi);
+		nodesWithinCircle = setdiff(nodesWithinCircle, pickedNodeCache_(end-1:end));		
+	elseif 1==nargin
+		if isempty(pickedNodeCache_)
+			warning('There MUST be at lease ONE Picked Nodes Available!'); return;
+		end	
+		if strcmp(eleType_.eleName, 'Shell133') || strcmp(eleType_.eleName, 'Shell144')
+			boundaryNodeCoords = nodeCoords_;
+		else
+			boundaryNodeCoords = nodeCoords_(boundaryNodes_,:);
+		end
+		ctr = boundaryNodeCoords(pickedNodeCache_(end),:);
+		radi = varargin{1};
+		nodesWithinCircle = find(vecnorm(ctr-boundaryNodeCoords,2,2)<=radi);
+		nodesWithinCircle = setdiff(nodesWithinCircle, pickedNodeCache_(end));		
 	else
-		boundaryNodeCoords = nodeCoords_(boundaryNodes_,:);
+		warning('Wrong Input!'); return;
 	end
-	ctr = boundaryNodeCoords(pickedNodeCache_(end-1),:);
-	radi = norm(ctr-boundaryNodeCoords(pickedNodeCache_(end),:));
-	nodesWithinCircle = find(vecnorm(ctr-boundaryNodeCoords,2,2)<=radi);
-	nodesWithinCircle = setdiff(nodesWithinCircle, pickedNodeCache_(end-1:end));
+
 	if strcmp(eleType_.eleName, 'Plane133') || strcmp(eleType_.eleName, 'Plane144')
 		hdPickedNode_(end+1) = plot(boundaryNodeCoords(nodesWithinCircle,1), ...
 			boundaryNodeCoords(nodesWithinCircle,2), 'xr', 'LineWidth', 2, 'MarkerSize', 8);	
--- src/Common/PickByLine.m
+++ src/Common/PickByLine.m
@@ -1,16 +1,17 @@
-function PickByLine(constantDir, varargin)
+function PickByLine(constantDir, opt, varargin)
 	global eleType_;
 	global nodeCoords_;
 	global boundaryNodes_;
 	global pickedNodeCache_;
 	global hdPickedNode_;
 	
-	if 1==nargin, lineRelaxFactor = 0; else, lineRelaxFactor = varargin{1}; end
+	if 2==nargin, lineRelaxFactor = 0; else, lineRelaxFactor = varargin{1}; end
 	dcm_obj = datacursormode;
 	info_struct = getCursorInfo(dcm_obj);
 	if isempty(info_struct)
 		warning('No Cursor Mode Available!'); return;
 	end
+	
 	tarNode = info_struct.Position;
 	if strcmp(eleType_.eleName, 'Shell133') || strcmp(eleType_.eleName, 'Shell144')
 		boundaryNodeCoords = nodeCoords_;
@@ -21,14 +22,27 @@ function PickByLine(constantDir, varargin)
 	hold on; 
 	if strcmp(eleType_.eleName, 'Plane133') || strcmp(eleType_.eleName, 'Plane144')
 		if 1~=length(constantDir), error('Wrongly Defined Line Direction!'); end
+		if ~(1==opt || 0==opt || -1==opt), warning('Wrong Input!'); return; end
 		switch constantDir
 			case 'X'
-				nodesOnLine = find(abs(boundaryNodeCoords(:,1)-tarNode(1))<=lineRelaxFactor);
+				% nodesOnLine = find(abs(boundaryNodeCoords(:,1)-tarNode(1))<=lineRelaxFactor);
+				refNodePool = boundaryNodeCoords(:,1);
+				refPos = tarNode(1);				
 			case 'Y'
-				nodesOnLine = find(abs(boundaryNodeCoords(:,2)-tarNode(2))<=lineRelaxFactor);
+				% nodesOnLine = find(abs(boundaryNodeCoords(:,2)-tarNode(2))<=lineRelaxFactor);
+				refNodePool = boundaryNodeCoords(:,2);
+				refPos = tarNode(2);					
 			otherwise
 				error('Wrongly Defined Line Direction!');
-		end	
+		end
+		switch opt
+			case 1
+				nodesOnLine = find(refNodePool>=refPos);
+			case 0
+				nodesOnLine = find(abs(refNodePool-refPos)<=lineRelaxFactor);
+			case -1
+				nodesOnLine = find(refNodePool<=refPos);
+		end
 		hdPickedNode_(end+1) = plot(boundaryNodeCoords(nodesOnLine,1), ...
 			boundaryNodeCoords(nodesOnLine,2), 'xr', 'LineWidth', 2, 'MarkerSize', 8);
 	else
